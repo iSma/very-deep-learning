e8b33cc5cff3e61912ffbd7d2f3d9f88a10e4967
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2017-11-17 20:59:14 +0100

    Remove subclasses of BaseDataset (now DrawDataset)
    Specialized preprocessing is now available through transformers

--- deepdraw/data/loader.py
+++ deepdraw/data/loader.py
@@ -6,15 +6,16 @@ from torch.utils.data import Dataset
 from .unpack import load
 
 
-class BaseDataset(Dataset):
-    def __init__(self, path, index):
+class DrawDataset(Dataset):
+    def __init__(self, path, index, transform=None):
         self.path = path
         self.index = index
+        self.transform = transform
 
     @classmethod
-    def load(self, path):
+    def load(self, path, transform=None):
         index = pd.read_csv(join(path, 'index.csv'))
-        return self(path, index)
+        return self(path, index, transform)
 
     def reduce(self, n, seed=None):
         """Reduce dataset to n drawings per class
@@ -38,8 +39,7 @@ class BaseDataset(Dataset):
         res = np.concatenate(res)
         index = self.index.iloc[res]
 
-        # return new instance of same class
-        return type(self)(self.path, index)
+        return type(self)(self.path, index, self.transform)
 
     def split(self, sizes):
         """Splits the dataset into subsets of relative size given by `sizes`.
@@ -83,86 +83,9 @@ class BaseDataset(Dataset):
 
     def __getitem__(self, i):
         word, offset = self.index.word[i], self.index.offset[i]
-        return load(self.path, word, offset), word
+        drawing = load(self.path, word, offset)
 
+        if self.transform:
+            drawing = self.transform(drawing)
 
-class StrokeDataset(BaseDataset):
-    def __getitem__(self, i):
-        drawing, word = super().__getitem__(i)
-        return drawing['drawing'], word
-
-
-class RasterDataset(BaseDataset):
-    # TODO: add transformation
-    def __init__(self, path, index, center=True):
-        super().__init__(path, index)
-        self.center = center
-
-    def __getitem__(self, i):
-        drawing, word = super().__getitem__(i)
-        return rasterize(drawing, self.center), word
-
-
-def rasterize(drawing, center=False):
-    """Rasterize drawing using Bresenham's line algorithm.
-
-    Args:
-        drawing (dict): drawing to rasterize, as returned by unpack_drawing()
-        center (bool): move drawing to center resulting image
-    Returns:
-        numpy.ndarray: a 256x256 boolean numpy array
-    """
-
-    # TODO: resize image?
-
-    drawing = drawing['drawing']
-    if center:
-        x_max = max(x for X, _ in drawing for x in X)
-        y_max = max(y for _, Y in drawing for y in Y)
-
-    image = np.zeros((256, 256), dtype=np.bool)
-    for stroke in drawing:
-        # stroke = [(x0, x1, ...), (y0, y1, ...)]
-        # -> [(x0, y0), (x1, y1), ...]
-        stroke = [*zip(*stroke)]
-        # -> [((x0, y0), (x1, y1)),  ((x1, y1), (x2, y2)), ...]
-        stroke = [*zip(stroke[:-1], stroke[1:])]
-
-        for (x0, y0), (x1, y1) in stroke:
-            if center:
-                dx = 256 - x_max
-                dy = 256 - y_max
-                x0 += dx // 2
-                x1 += dx // 2
-                y0 += dy // 2
-                y1 += dy // 2
-
-            dx = abs(x1 - x0)
-            dy = abs(y1 - y0)
-            sx = -1 if x0 > x1 else 1
-            sy = -1 if y0 > y1 else 1
-
-            x, y = x0, y0
-            if dx > dy:
-                err = dx / 2
-                while x != x1:
-                    image[x, y] = True
-                    err -= dy
-                    if err < 0:
-                        y += sy
-                        err += dx
-                    x += sx
-            else:
-                err = dy / 2
-                while y != y1:
-                    image[x, y] = True
-                    err -= dx
-                    if err < 0:
-                        x += sx
-                        err += dy
-                    y += sy
-
-            image[x, y] = True
-
-    image = image.T  # Transpose to switch x and y axes
-    return image
+        return drawing, word
---
+++ deepdraw/data/transform.py
@@ -0,0 +1,122 @@
+import numpy as np
+
+
+class Sequencer(object):
+    """Converts a drawing to sequence of state vectors.
+
+    (dx, dy, p1, p2, p3)
+    p1: Pen is down
+    p2: Pen is up (next line won't be drawn)
+    p3: End of drawing
+
+    See https://arxiv.org/abs/1704.03477
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, drawing):
+        """
+        Args:
+            drawing (dict): drawing to be sequenced
+        Returns:
+            list of tuples: (dx, dy, p1, p2, p3)
+        """
+        drawing = drawing['drawing']
+        strokes = []
+        for stroke in drawing:
+            # stroke = [(x0, x1, ...), (y0, y1, ...)]
+            # -> [(x0, y0, 1, 0, 0), (x1, y1, 1, 0, 0), ...]
+            stroke = [*zip(*stroke)]
+            stroke = [(x, y, 1, 0, 0) for x, y in stroke]
+            # Mark last point as end of stroke
+            stroke[-1] = stroke[-1][:2] + (0, 1, 0)
+            strokes += stroke
+
+        # Duplicate first point
+        strokes = strokes[:1] + strokes
+        # Pair adjacent points together: [(p0, p1), (p1, p2), ...]
+        strokes = [*zip(strokes[:-1], strokes[1:])]
+
+        res = []
+        for a, b in strokes:
+            (x0, y0), (x1, y1) = a[:2], b[:2]
+            dx, dy = x1-x0, y1-y0
+
+            res.append((dx, dy) + b[2:])
+
+        # Add end-of-drawing marker
+        res.append((0, 0, 0, 0, 1))
+        return res
+
+
+class Rasterizer(object):
+    """Rasterize drawing using Bresenham's line algorithm.
+
+    Args:
+        center (bool): move drawing to center resulting image
+    """
+
+    def __init__(self, center=True):
+        # TODO: resize image?
+        self.center = center
+
+    def __call__(self, drawing):
+        """
+        Args:
+            drawing (dict): drawing to be rasterized
+        Returns:
+            numpy.ndarray: a 256x256 boolean numpy array
+        """
+
+        drawing = drawing['drawing']
+        if self.center:
+            x_max = max(x for X, _ in drawing for x in X)
+            y_max = max(y for _, Y in drawing for y in Y)
+
+        image = np.zeros((256, 256), dtype=np.bool)
+        for stroke in drawing:
+            # stroke = [(x0, x1, ...), (y0, y1, ...)]
+            # -> [(x0, y0), (x1, y1), ...]
+            stroke = [*zip(*stroke)]
+            # -> [((x0, y0), (x1, y1)),  ((x1, y1), (x2, y2)), ...]
+            stroke = zip(stroke[:-1], stroke[1:])
+
+            for (x0, y0), (x1, y1) in stroke:
+                if self.center:
+                    dx = 256 - x_max
+                    dy = 256 - y_max
+                    x0 += dx // 2
+                    x1 += dx // 2
+                    y0 += dy // 2
+                    y1 += dy // 2
+
+                dx = abs(x1 - x0)
+                dy = abs(y1 - y0)
+                sx = -1 if x0 > x1 else 1
+                sy = -1 if y0 > y1 else 1
+
+                x, y = x0, y0
+                if dx > dy:
+                    err = dx / 2
+                    while x != x1:
+                        image[x, y] = True
+                        err -= dy
+                        if err < 0:
+                            y += sy
+                            err += dx
+                        x += sx
+                else:
+                    err = dy / 2
+                    while y != y1:
+                        image[x, y] = True
+                        err -= dx
+                        if err < 0:
+                            x += sx
+                            err += dy
+                        y += sy
+
+                image[x, y] = True
+
+        image = image.T  # Transpose to switch x and y axes
+        return image
